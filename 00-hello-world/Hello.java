7148250444f64020e55d756861721e81485e4760
Raymond <raymondf@raymonds-air.78b505.nycboe.org>
2021-09-27 08:18:10 -0400

    week 2 work

---
+++ 00-hello-world/Hello.java
@@ -0,0 +1,7 @@
+public class Hello {
+  public static void main(String[] args) {
+    //generates some simple output
+    System.out.println("Hello, World!"); //prints out my text "hello world!"
+
+  } //closes main program
+} //closes class 
---
+++ 01-perfect-day/PerfectDay.java
@@ -0,0 +1,48 @@
+public class PerfectDay {
+  public static void main(String[] args) {
+    System.out.println("My perfect day is, ");
+
+  Morning();
+  Afternoon ();
+  Night ();
+  }
+//main method
+
+  public static void Morning() {
+    System.out.print("During the morning ");
+    System.out.println("I sleep all day until about 12:30 pm.");
+    System.out.println("I eat a good breakfast that my mom had made me while watching tv.");
+    System.out.println("I get ready and change into clothes to go outside.");
+  } //morning done
+
+  public static void Afternoon() {
+    System.out.println("During the afternoon, ");
+    System.out.print("I am outside with my friends just having fun and hanging out.");
+    System.out.println("I then get a bike and ride it around the whole city with my friends.");
+    System.out.println("We also eat some food together.");
+    }
+
+  public static void Night() {
+    System.out.println("During the night, ");
+    System.out.print("I go home to find out the my family and I are going to a party.");
+    System.out.println("I get to the party and have a ton of fun and eat a bunch of pizza.");
+    System.out.println("The party also lasts a very long time.");
+    }
+}
+/* Morning
+I sleep all day until about 12:30 pm.
+I eat a good breakfast that my mom had made me while watching tv.
+I get ready and change into clothes to go outside.
+*/
+
+/* afternoon
+I am outside with my friends just having fun and hanging out.
+I then get a bike and ride it around the whole city with my friends.
+We also eat some food together.
+*/
+
+/* Night
+i go home to find out the my family and I are going to a party.
+I get to the party and have a ton of fun and eat a bunch of pizza.
+The party also lasts a very long time.
+*/
