83b225e5d5d1308db65cb5f99376e1d081dea7a2
Raymond <raymondf@raymonds-air.78b505.nycboe.org>
2021-11-04 08:21:56 -0400

    Changes we made on Coordinate plane.

--- 03 - Guessing/Guess.java
+++ 03 - Guessing/Guess.java
@@ -5,13 +5,13 @@ import java.util.Scanner;
 public class Guess {
   public static void main(String[] args) {
     Random Random_number= new Random();
-    int answer=Random_number.nextInt(20);
-    int turns=10;
+    int answer=Random_number.nextInt(20); // chooses random number from 1 through 20 for player to guess
+    int turns=0;
     Scanner scan=new Scanner(System.in);
     System.out.println("Guess a number between 1 to 20, You will have 10 trys!" );
 
     int guess;
-    int i=10;
+    int i=11;
     boolean win=false;
     while(win==false) {
       guess=scan.nextInt();
@@ -20,28 +20,28 @@ public class Guess {
     if(guess==answer) {
       win=true;
     }
-    else if(i<0){
-      System.out.println("You lose! the right answer was: "+answer);
+    else if(i<3){ //if turns is less than 0 then player loses and will be told the correct number
+      System.out.println("You lose! the right answer was: "+answer+" ");
+      System.out.println("The correct number was "+answer+" ");
       return;
     }
-    else if(guess<answer){
-      i++;
-      System.out.println("Your guess is lower than the number! Turns left: "+(i-1));
+    else if(guess<answer){ //checks if players guess is lower than the correct guess. Also informs player if guess is too low.
+      i--;
+      System.out.println("Your guess is lower than the correct number! Turns left: "+(i-1));
 
 
 
     }
-    else if(guess>answer) {
-      i++;
-      System.out.println("Your guess is Higher than the answer! Turns left: "+(i-1));
+    else if(guess>answer) { // checks if players guess is higher than the correct guess. Also informs player if guess is too high.
+      i--;
+      System.out.println("Your guess is higher than the correcr number! Turns left: "+(i-1));
 
     }
 
 
   }
     System.out.println("You win!");
-    System.out.println("Then number was "+answer);
-    System.out.println("You used "+turns+" turns to guess the right number.");
+    System.out.println("You used "+turns+" trys to guess the right number. Good job!");
 }
 
 }
---
+++ Username/Username.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Username {
+  public static void main (String[] args) {
+    //create an instance of the new scanner
+    Scanner s = new Scanner (System.in);
+
+    //ask user their first name
+    System.out.print("Please enter your first name: ");
+    String fName = s.nextLine();
+
+
+
+
+
+
+
+  }
+
+
+
+
+
+}
